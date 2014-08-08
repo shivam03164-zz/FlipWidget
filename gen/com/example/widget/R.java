51ab05722e9d67c5afbd6f280f62b52e69b22e82
Shivam Jaiswal <shivam.jaiswal@flipkart.com>
2014-08-08 06:37:03 +0530

    shivam latest commit

--- gen/com/example/widget/R.java
+++ gen/com/example/widget/R.java
@@ -59,7 +59,8 @@ public final class R {
         public static final int voiceInput=0x7f090006;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_layout=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f080000;
--- src/com/example/widget/MainActivity.java
+++ src/com/example/widget/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.widget;
 
+import java.util.ArrayList;
+
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -17,7 +22,7 @@ import android.widget.RemoteViews;
 import android.widget.Toast;
 
 public class MainActivity extends AppWidgetProvider{
-
+	protected static final int RESULT_SPEECH = 1;
    @Override
    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
    int[] appWidgetIds) {
@@ -26,6 +31,7 @@ public class MainActivity extends AppWidgetProvider{
       String url = "http://m.flipkart.com";
       String url2 = "http://m.flipkart.com/search";
       String url3 = "http://m.flipkart.com/offers";
+      String url4 = "http://m.flipkart.com/search?q=puma";
       
       
       Intent intent = new Intent(Intent.ACTION_VIEW);
@@ -41,7 +47,20 @@ public class MainActivity extends AppWidgetProvider{
       intent3.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
       intent3.setData(Uri.parse(url3));
       
-
+      
+      Intent intent4 = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+      intent4.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+      
+      
+      Intent intent5 = new Intent(context, VoiceActivity.class);
+      intent5.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
+      
+      
+      
+      
+      
+      //intent5.setData(Uri.parse(url4));
+      
       
       PendingIntent pending1 = PendingIntent.getActivity(context, 0,
       intent, 0);
@@ -53,6 +72,13 @@ public class MainActivity extends AppWidgetProvider{
       PendingIntent pending3 = PendingIntent.getActivity(context, 0,
     	      intent3, 0);
       
+      PendingIntent pending4 = PendingIntent.getActivity(context, 0,
+    	      intent4, 0);
+      
+      PendingIntent pending5 = PendingIntent.getActivity(context, 0,
+    	      intent5, 0);
+      
+      
       
       RemoteViews views = new RemoteViews(context.getPackageName(),
       R.layout.activity_main);
@@ -61,6 +87,9 @@ public class MainActivity extends AppWidgetProvider{
       views.setOnClickPendingIntent(R.id.flipLogo, pending1);
       views.setOnClickPendingIntent(R.id.search, pending2);
       views.setOnClickPendingIntent(R.id.item_button, pending3);
+      views.setOnClickPendingIntent(R.id.barcode, pending4);
+      views.setOnClickPendingIntent(R.id.voiceInput, pending5);
+      
       
       
       
@@ -69,5 +98,9 @@ public class MainActivity extends AppWidgetProvider{
       
       Toast.makeText(context, "widget added", Toast.LENGTH_SHORT).show();	
       }
-   }	
+      
+      
+      
+   }   
+   
 }
